33f6dd9dfdcb3e360247c46845bd1c8472866c91
lunaskyhy <lunasky401@gmail.com>
2025-05-24 21:36:27 +0900

    Speed limit 表示の変更

--- app/src/main/java/com/lunaskyhy/harukaroute/car/MainCarSession.kt
+++ app/src/main/java/com/lunaskyhy/harukaroute/car/MainCarSession.kt
@@ -2,8 +2,15 @@ package com.lunaskyhy.harukaroute.car
 
 import android.content.Intent
 import android.util.Log
+import androidx.car.app.CarContext
 import androidx.car.app.Screen
 import androidx.car.app.Session
+import androidx.car.app.model.MessageTemplate
+import androidx.car.app.model.PaneTemplate
+import androidx.car.app.model.Template
+import androidx.car.app.navigation.model.MapTemplate
+import androidx.car.app.navigation.model.NavigationTemplate
+import androidx.car.app.navigation.model.NavigationTemplate.NavigationInfo
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import com.mapbox.android.core.permissions.PermissionsManager
@@ -11,11 +18,15 @@ import com.mapbox.maps.ContextMode
 import com.mapbox.maps.MapInitOptions
 import com.mapbox.maps.MapOptions
 import com.mapbox.maps.extension.androidauto.MapboxCarMap
+import com.mapbox.maps.extension.androidauto.MapboxCarMapSurface
 import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
 import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.base.speed.model.SpeedLimitSign
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
 import com.mapbox.navigation.ui.androidauto.MapboxCarContext
 import com.mapbox.navigation.ui.androidauto.deeplink.GeoDeeplinkNavigateAction
+import com.mapbox.navigation.ui.androidauto.navigation.CarNavigationInfoProvider
+import com.mapbox.navigation.ui.androidauto.navigation.speedlimit.SpeedLimitOptions
 import com.mapbox.navigation.ui.androidauto.notification.MapboxCarNotificationOptions
 import com.mapbox.navigation.ui.androidauto.screenmanager.MapboxScreen
 import com.mapbox.navigation.ui.androidauto.screenmanager.MapboxScreenManager
@@ -33,6 +44,10 @@ class MainCarSession : Session() {
         mapboxCarContext.customize {
             notificationOptions = MapboxCarNotificationOptions.Builder()
                 .startAppService(MainCarAppService::class.java)
+                .build()
+            speedLimitOptions = SpeedLimitOptions.Builder()
+                .forcedSignFormat(SpeedLimitSign.VIENNA)
+                .warningThreshold(20)
                 .build()
         }
 
@@ -65,7 +80,6 @@ class MainCarSession : Session() {
         val firstScreenKey = if (PermissionsManager.areLocationPermissionsGranted(carContext)) {
             MapboxScreenManager.current()?.key ?: MapboxScreen.FREE_DRIVE
         } else {
-
             MapboxScreen.NEEDS_LOCATION_PERMISSION
         }
 
@@ -79,4 +93,3 @@ class MainCarSession : Session() {
         GeoDeeplinkNavigateAction(mapboxCarContext).onNewIntent(intent)
     }
 }
-
